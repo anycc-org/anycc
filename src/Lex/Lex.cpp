1f38f78107cd64075fb042e51cdf87d283836844
bazina <youssefbazina7@gmail.com>
2023-12-05 12:45:54 +0200

    ANYCC-8 - Use ready components in Lex

--- src/Lex/Lex.cpp
+++ src/Lex/Lex.cpp
@@ -1,4 +1,5 @@
 #include <Lex/Lex.h>
+#include "NFAGenerator.h"
 
 
 Lex::~Lex() {
@@ -11,9 +12,21 @@ Lex::Lex(std::string *rules_file_name, std::string *program_file_name) {
     this->program_file_name = program_file_name;
 }
 
+void Lex::buildLex() {
+    read_rules();
+    NFA *nfa = buildNFA();
+    nfa->printNFA();
+}
+
 void Lex::read_rules() {
     rules = new Rules();
     inputReader = new InputReader(this->rules_file_name, rules);
 
     rules->printRules();
+}
+
+NFA *Lex::buildNFA() {
+    auto *NfaGenerator = new NFAGenerator();
+    return NfaGenerator->buildNFA(rules->getRegularExpressions(), rules->getRegularDefinitions(), rules->getKeywords(),
+                                  rules->getPunctuations());
 }
