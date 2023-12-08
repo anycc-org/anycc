28c375d71f32da1dcca003163e28b5a81d39be12
bazina <youssefbazina7@gmail.com>
2023-12-08 21:02:24 +0200

    ANYCC-38 - Modifying Prints

--- src/Lex/Lex.cpp
+++ src/Lex/Lex.cpp
@@ -21,43 +21,36 @@ void Lex::buildLex() {
     NFA *nfa = buildNFA();
     TransitionDiagram *table = new TransitionDiagram(nfa->getStartState(), nfa->getEndStates(), rules->getTokens(),
                                                      rules->getTokensPriority());
-    // table->print();
-    std::cout << table->getStates().size() << "\n";
-    std::cout << table->getEndStates().size() << "\n";
-    std::cout << table->getDeadStates().size() << "\n";
+
+    std::cout << "Converted to NFA\n";
+    std::cout << "Number of States = " << table->getStates().size() << "\n";
+    std::cout << "Number of End States = " << table->getEndStates().size() << "\n";
+    std::cout << "Number of Dead States = " << table->getDeadStates().size() << "\n";
     table->toDotFile("nfa.dot");
     table->toCSVFile("nfa.csv");
     table->toMDFile("nfa.md");
+
     DeterministicTransitionDiagramCreator dfaCreator;
     table = dfaCreator.subsetConstruction(table);
-    std::cout << "converted to dfa\n";
+    std::cout << "Converted to DFA\n";
     table->toDotFile("dfa.dot");
     table->toCSVFile("dfa.csv");
     table->toMDFile("dfa.md");
-    // table->print();
-    std::cout << table->getStates().size() << "\n";
-    std::cout << table->getEndStates().size() << "\n";
-    std::cout << table->getDeadStates().size() << "\n";
+
+    std::cout << "Number of States = " << table->getStates().size() << "\n";
+    std::cout << "Number of End States = " << table->getEndStates().size() << "\n";
+    std::cout << "Number of Dead States = " << table->getDeadStates().size() << "\n";
     TransitionDiagramMinimizer minimizer;
     table = minimizer.minimize(table);
     table->toDotFile("min_dfa.dot");
     table->toCSVFile("min_dfa.csv");
     table->toMDFile("min_dfa.md");
-    std::cout << "minimized dfa\n";
-    // table->print();
-    std::cout << table->getStates().size() << "\n";
-    std::cout << table->getEndStates().size() << "\n";
-    std::cout << table->getDeadStates().size() << "\n";
-    for (auto kv: table->getEndStatesTokensMap()) {
-        std::cout << kv.first->getStateId() << ": " << kv.second << "\n";
-    }
-    std::cout << table->getEndStatesTokensMap().size();
-    std::cout << "\n";
-    for (auto s: rules->getTokens()) {
-        std::cout << s << "\n";
-    }
-    std::cout << rules->getTokens().size();
-    std::cout << "\n";
+
+    std::cout << "Minimized DFA\n\n";
+
+    std::cout << "Number of States = " << table->getStates().size() << "\n";
+    std::cout << "Number of End States = " << table->getEndStates().size() << "\n";
+    std::cout << "Number of Dead States = " << table->getDeadStates().size() << "\n\n\n";
 
     Analyzer analyzer(*program_file_name, table->getStartState(), table);
     analyzer.analyzeProgram();
