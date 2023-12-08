727fd6be562eec2a32f10aaeb068e5de8c8de0c5
bazina <youssefbazina7@gmail.com>
2023-12-08 21:12:00 +0200

    ANYCC-38 - Reformatting DeterministicTransitionDiagramCreator files and make two methods static

--- include/Lex/DeterministicTransitionDiagramCreator.h
+++ include/Lex/DeterministicTransitionDiagramCreator.h
@@ -11,13 +11,13 @@ public:
     /**
      * @brief do subset construction on
       the Transition Diagram and return a pointer to the  Transition Diagram
-     * @warning incase of inplace was false, the deletion of the returned Transition Diagram Pointer is the responsibilty of 
+     * @warning in case of inplace was false, the deletion of the returned Transition Diagram Pointer is the responsibility of
      the caller  
      * @param inplace if true the same pointer passed will be returned, no new object created, otherwise, a new Transition Diagram will be created. 
      * @return TransitionDiagram* Transition Diagram pointer , see warning
      */
-    TransitionDiagram *subsetConstruction(TransitionDiagram *transdig, bool inplace = true);
+    static TransitionDiagram *subsetConstruction(TransitionDiagram *transition_diagram, bool inplace = true);
 
 private:
-    TransitionDiagram *subsetConstructionInplace(TransitionDiagram *transdig);
+    static TransitionDiagram *subsetConstructionInplace(TransitionDiagram *transition_diagram);
 };
