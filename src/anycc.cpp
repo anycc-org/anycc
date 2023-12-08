2c0b7294bdda3486de65f6c3717b7f6f258962b0
bazina <youssefbazina7@gmail.com>
2023-12-08 21:38:36 +0200

    ANYCC-38 - Using Lex functions

--- src/anycc.cpp
+++ src/anycc.cpp
@@ -5,6 +5,10 @@
 int main() {
     auto *rules_file_name = new std::string("../rules.txt");
     auto *program_file_name = new std::string("../program.txt");
+
     Lex *lex = new Lex(rules_file_name, program_file_name);
     lex->buildLex();
+
+    lex->getAllTokensAndCreateOutputFile();
+    lex->printSymbolTable();
 }
