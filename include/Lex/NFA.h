c84e50f08e3b6e5ef2e22d274d5f94a25076e086
bazina <youssefbazina7@gmail.com>
2023-12-08 21:40:20 +0200

    ANYCC-38 - Only reformatting files

--- include/Lex/NFA.h
+++ include/Lex/NFA.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <vector>
 #include <unordered_map>
 #include <set>
@@ -8,28 +9,44 @@
 class NFA {
 public:
     NFA();
-    NFA(NFAState* start, NFAState* end);
-    NFA(const NFA& other) noexcept;
+
+    NFA(NFAState *start, NFAState *end);
+
+    NFA(const NFA &other) noexcept;
+
     ~NFA();
 
-    NFAState* getStartState() const;
-    NFAState* getEndState() const;
-    std::vector<const NFAState*> getEndStates();
+    NFAState *getStartState() const;
+
+    NFAState *getEndState() const;
+
+    std::vector<const NFAState *> getEndStates();
+
     std::string getTokenName() const;
-    void setTokenName(const std::string& name);
-    void addEndState(NFAState* state);
-    static NFA* basicCharToNFA(char c);
-    static NFA* wordToNFA(const std::string& word);
-    static NFA* unionNAFs(NFA* nfa1, NFA* nfa2);
-    static NFA* concatNAFs(NFA* nfa1, NFA* nfa2);
-    static NFA* kleeneStarNFA(NFA* nfa);
-    static NFA* positiveClosureNFA(NFA* nfa);
-    static NFA* unionRangeNFAs(NFA* rangeStartNFA, NFA* rangeEndNFA);
+
+    void setTokenName(const std::string &name);
+
+    void addEndState(NFAState *state);
+
+    static NFA *basicCharToNFA(char c);
+
+    static NFA *wordToNFA(const std::string &word);
+
+    static NFA *unionNAFs(NFA *nfa1, NFA *nfa2);
+
+    static NFA *concatNAFs(NFA *nfa1, NFA *nfa2);
+
+    static NFA *kleeneStarNFA(NFA *nfa);
+
+    static NFA *positiveClosureNFA(NFA *nfa);
+
+    static NFA *unionRangeNFAs(NFA *rangeStartNFA, NFA *rangeEndNFA);
+
     void printNFA() const;
 
 private:
     std::string tokenName;
-    NFAState* startState;
-    NFAState* endState;
-    std::vector<NFAState*> endStates;
+    NFAState *startState;
+    NFAState *endState;
+    std::vector<NFAState *> endStates;
 };
