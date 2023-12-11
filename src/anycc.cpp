49af95177ce79d52f444e001f6a9139a0e35a0a1
AbdelrahmanMosly <95633556+AbdelrahmanMosly@users.noreply.github.com>
2023-12-11 16:29:49 +0200

    ANYCC-39 add reading CFG in utilites.h

--- src/anycc.cpp
+++ src/anycc.cpp
@@ -6,57 +6,10 @@
 #include <set>
 #include <unordered_map>
 #include <vector>
-// Function to parse the CFG input file
-std::unordered_map<std::string, std::vector<std::vector<std::string>>> parseCFGInput(const std::string& filename) {
-  std::ifstream inputFile(filename);
-  std::unordered_map<std::string, std::vector<std::vector<std::string>>> grammar;
-
-  if (inputFile.is_open()) {
-    std::string line;
-    std::string currentNonTerminal;
-
-    while (std::getline(inputFile, line)) {
-      // Skip comments and empty lines
-      if (line.empty() || line[0] == '#')
-        continue;
-
-      // Find the position of ::= in the line
-      size_t arrowPos = line.find("::=");
-      if (arrowPos != std::string::npos) {
-        // Extract the non-terminal before ::= as the current non-terminal
-        currentNonTerminal = line.substr(0, arrowPos);
-
-        // Tokenize the RHS of the production rule using '|'
-        line = line.substr(arrowPos + 3); // Move past "::="
-        std::istringstream ss(line);
-        std::string token;
-        std::vector<std::string> production;
-
-        while (ss >> token) {
-          if (token == "|") {
-            // Start a new production for the same non-terminal
-            grammar[currentNonTerminal].push_back(production);
-            production.clear();
-          } else {
-            production.push_back(token);
-          }
-        }
-
-        // Add the last production to the grammar
-        grammar[currentNonTerminal].push_back(production);
-      }
-    }
-
-    inputFile.close();
-  } else {
-    std::cerr << "Unable to open file: " << filename << std::endl;
-  }
-
-  return grammar;
-}
+#include "Utilities.h"
 
 int main() {
-  std::unordered_map<std::string, std::vector<std::vector<std::string>>> grammar =parseCFGInput("../CFG.txt");
+  std::unordered_map<std::string, std::vector<std::vector<std::string>>> grammar =Utilities::parseCFGInput("../CFG.txt");
   FirstAndFollowGenerator grammarChecker(grammar);
   grammarChecker.isLL1Grammar();
   // Print firstSets
