e9cdd543235dd6bb25d6e11ff0b88728f074d3d5
bazina <youssefbazina7@gmail.com>
2023-12-18 14:38:31 +0200

    ANYCC-27 - Test predictive table class

--- src/anycc.cpp
+++ src/anycc.cpp
@@ -7,37 +7,46 @@
 #include <unordered_map>
 #include <vector>
 #include "Utilities.h"
+#include "Parser/PredictiveTable.h"
 
 int main() {
-  std::unordered_map<std::string, std::vector<std::vector<std::string>>> grammar =Utilities::parseCFGInput("../CFG.txt");
-  FirstAndFollowGenerator firstAndFollowGenerator(grammar);
-  firstAndFollowGenerator.compute();
-  // Print first_sets
-  const auto& first_sets = firstAndFollowGenerator.getFirstSets();
-  std::cout << "First Sets:\n";
-  for (const auto& entry : first_sets) {
-    const std::string&non_terminal = entry.first;
-    const std::set<std::string>&first_set = entry.second;
+    std::unordered_map<std::string, std::vector<std::vector<std::string>>> grammar = Utilities::parseCFGInput(
+            "../CFG.txt");
+    FirstAndFollowGenerator firstAndFollowGenerator(grammar);
+    firstAndFollowGenerator.compute();
+    // Print first_sets
+    const auto &first_sets = firstAndFollowGenerator.getFirstSets();
+    std::cout << "First Sets:\n";
+    for (const auto &entry: first_sets) {
+        const std::string &non_terminal = entry.first;
+        const std::set<std::pair<std::string, Production>, CompareFirst> &first_set = entry.second;
 
-    std::cout << non_terminal << ": { ";
-    for (std::string symbol : first_set) {
-      std::cout << symbol << ' ';
+        std::cout << non_terminal << ": { ";
+        for (std::pair<std::string, Production> symbol: first_set) {
+            std::cout << symbol.first << ' ';
+        }
+        std::cout << "}\n";
     }
-    std::cout << "}\n";
-  }
 
-  // Print followSets
-  const auto& followSets = firstAndFollowGenerator.getFollowSets();
-  std::cout << "\nFollow Sets:\n";
-  for (const auto& entry : followSets) {
-    const std::string&non_terminal = entry.first;
-    const std::set<std::string>&follow_set = entry.second;
+    // Print followSets
+    const auto &followSets = firstAndFollowGenerator.getFollowSets();
+    std::cout << "\nFollow Sets:\n";
+    for (const auto &entry: followSets) {
+        const std::string &non_terminal = entry.first;
+        const std::set<std::string> &follow_set = entry.second;
 
-    std::cout << non_terminal << ": { ";
-    for (std::string symbol : follow_set) {
-      std::cout << symbol << ' ';
+        std::cout << non_terminal << ": { ";
+        for (std::string symbol: follow_set) {
+            std::cout << symbol << ' ';
+        }
+        std::cout << "}\n";
     }
-    std::cout << "}\n";
-  }
-  return 0;
+
+    // build predictive table
+    PredictiveTable predictiveTable(first_sets, followSets, firstAndFollowGenerator.getNonTerminals());
+    predictiveTable.buildPredictiveTable();
+    std::cout << "\nPredictive Table:\n";
+
+
+    return 0;
 }
