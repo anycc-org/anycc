cbc7d465b4aab916e2c8c59c2444a9d349ba810e
AbdelrahmanMosly <95633556+AbdelrahmanMosly@users.noreply.github.com>
2023-12-21 19:05:21 +0200

    Anycc 39 reading CFG rules as phase 2 require (#33)
    * ANYCC-39 reading CFG file as phase 2 convention
    
    * ANYCC-39 make sure production not empty before adding it
    
    * ANYCC-39 use lambda function
    
    * ANYCC-39 change start symbon in constans.h

--- src/Lex/Utilities.cpp
+++ src/Lex/Utilities.cpp
@@ -194,38 +194,53 @@ std::unordered_map<std::string, std::vector<std::vector<std::string>>> Utilities
     std::unordered_map<std::string, std::vector<std::vector<std::string>>> grammar;
 
     if (inputFile.is_open()) {
+
         std::string line;
         std::string currentNonTerminal;
 
+        auto processProduction = [&](const std::string& line) {
+          std::vector<std::string> production;
+          std::istringstream ss(line);
+          std::string token;
+
+          while (ss >> token) {
+            if (token == "|" && production.empty()) {
+              continue;
+            }
+            if (token == "|") {
+              // Start a new production for the same non-terminal
+              grammar[currentNonTerminal].push_back(production);
+              production.clear();
+            } else {
+              token = std::regex_replace(token, std::regex("'"), "");
+              production.push_back(token);
+            }
+          }
+          // Add the last production to the grammar
+          grammar[currentNonTerminal].push_back(production);
+        };
+
         while (std::getline(inputFile, line)) {
             // Skip comments and empty lines
-            if (line.empty() || line[0] == '#')
+            if (line.empty())
                 continue;
 
-            // Find the position of ::= in the line
-            size_t arrowPos = line.find("::=");
-            if (arrowPos != std::string::npos) {
-                // Extract the non-terminal before ::= as the current non-terminal
-                currentNonTerminal = line.substr(0, arrowPos);
+            std::vector<std::string> production;
+            if(line[0] == '#'){// new rule
+             // Find the position of = in the line
+              std::regex spacesBeforeArrow("\\s*\\=");
+              line = std::regex_replace(line, spacesBeforeArrow, "=");
+              size_t arrowPos = line.find("="); // first occurence
+              if (arrowPos != std::string::npos) {
+                  // Extract the non-terminal before ::= as the current non-terminal
+                  currentNonTerminal = line.substr(2, arrowPos-2); //skip '#' index 0 and ' ' index 1
 
-                // Tokenize the RHS of the production rule using '|'
-                line = line.substr(arrowPos + 3); // Move past "::="
-                std::istringstream ss(line);
-                std::string token;
-                std::vector<std::string> production;
-
-                while (ss >> token) {
-                    if (token == "|") {
-                      // Start a new production for the same non-terminal
-                      grammar[currentNonTerminal].push_back(production);
-                      production.clear();
-                    } else {
-                      production.push_back(token);
-                    }
-                }
-
-                // Add the last production to the grammar
-                grammar[currentNonTerminal].push_back(production);
+                  // Tokenize the RHS of the production rule using '|'
+                  line = line.substr(arrowPos + 1); // Move past "="
+                  processProduction(line);
+              }
+            }else{
+              processProduction(line);
             }
         }
 
