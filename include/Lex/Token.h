e4152aeae0cc47bc9486972755b3106f46d0b11f
bazina <youssefbazina7@gmail.com>
2023-12-24 12:36:36 +0200

    ANYCC 29 - Add option to have position for tokens

--- include/Lex/Token.h
+++ include/Lex/Token.h
@@ -2,6 +2,15 @@
 
 #include <string>
 
+struct Position {
+    int line_number;
+    int column_number;
+
+    Position(int line_number, int column_number) : line_number(line_number), column_number(column_number) {}
+
+    Position() = default;
+};
+
 class Token {
 public:
 
@@ -9,13 +18,18 @@ public:
 
     Token(std::string *key, std::string *value);
 
+    Token(std::string *key, std::string *value, Position *position);
+
     ~Token();
 
     std::string *getKey() const;
 
     std::string *getValue() const;
 
+    Position *getPosition() const;
+
 private:
     std::string *key{};
     std::string *value{};
+    Position *position{};
 };
