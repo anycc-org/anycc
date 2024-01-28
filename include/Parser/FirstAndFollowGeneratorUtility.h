7a25672158cedd99edecc2efcb3ec1cee6a74b67
bazina <youssefbazina7@gmail.com>
2024-01-28 23:14:39 +0200

    Anycc 55 Extract SubstringInfo so it is not repetitive

--- include/Parser/FirstAndFollowGeneratorUtility.h
+++ include/Parser/FirstAndFollowGeneratorUtility.h
@@ -2,18 +2,10 @@
 #define FIRSTANDFOLLOWGENERATORUTILITY_H
 
 #include "Parser/Production.h"
+#include "SubstringInfo.h"
 #include <set>
 #include <string>
 #include <vector>
-
-struct SubstringInfo {
-    int start;
-    int end;
-};
-
-inline bool compareSubstringInfo(const SubstringInfo &a, const SubstringInfo &b) {
-    return a.start < b.start;
-}
 
 std::vector<SubstringInfo>
 findAllLongestSubstringIndices(const std::string &input, const std::set<std::string> &substrings);
