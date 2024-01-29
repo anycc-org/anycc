ab5c830a8998adeb75cef498da1b4710c8d64c45
bazina <youssefbazina7@gmail.com>
2024-01-29 15:03:38 +0200

    Anycc 55 Remove unused functions

--- include/Parser/FirstAndFollowGeneratorUtility.h
+++ include/Parser/FirstAndFollowGeneratorUtility.h
@@ -7,13 +7,6 @@
 #include <string>
 #include <vector>
 
-std::vector<SubstringInfo>
-findAllLongestSubstringIndices(const std::string &input, const std::set<std::string> &substrings);
-
-bool isNT(const std::string &s, std::set<std::string> &nonTerminals);
-
 std::set<std::string> collectNonTerminals(const std::vector<Production> &grammar);
-
-std::string getLongestUpperCaseSequence(const std::string &str);
 
 #endif // FIRSTANDFOLLOWGENERATORUTILITY_H
